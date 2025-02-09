ec1eb19fbf565b64f7c08fc393ade4f68cdf71d5
Marchenko-Oleksiy <marchenkooleksiy8@gmail.com>
2025-02-09 12:49:14 +0200

    Update main.py

--- maze/main.py
+++ maze/main.py
@@ -12,6 +12,31 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_LEFT] and self.rect.x > 15:
+            self.rect.x -= self.speed
+        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+            self.rect.x += self.speed
+        if keys[K_UP] and self.rect.y > 15:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+class Enemy(GameSprite):
+    direction = "left"
+    def update(self):
+        if self.rect.x <= 470:
+            self.direction = "right"
+        if self.rect.x >= win_width-85:
+            self.direction = "left"
+
+        if self.direction == "left":
+            self.rect.x -= self.speed
+        if self.direction == "right":
+            self.rect.x += self.speed
+
 win_width = 700
 win_height = 500
 
@@ -19,11 +44,13 @@ window = display.set_mode((win_width, win_height))
 display.set_caption("Maze")
 background = transform.scale(image.load("background.jpg"), (win_width, win_height))
 
-player = GameSprite('hero.png', 5, win_height-80, 4)
-monster = GameSprite('cyborg.png', win_width-80, 280, 2)
+player = Player('hero.png', 5, win_height-80, 4)
+monster = Enemy('cyborg.png', win_width-80, 280, 2)
 final = GameSprite('treasure.png', win_width-120, win_height-80, 0)
 
 game = True
+finish = False
+
 clock = time.Clock()
 FPS = 60
 
@@ -36,9 +63,14 @@ while game:
         if e.type == QUIT:
             game = False
             
-    window.blit(background, (0, 0))
-    player.reset()
-    monster.reset()
+    if finish != True:
+        window.blit(background, (0, 0))
+        player.update()
+        monster.update()
+
+        final.reset()
+        player.reset()
+        monster.reset()
     
     display.update()
     clock.tick(FPS)
